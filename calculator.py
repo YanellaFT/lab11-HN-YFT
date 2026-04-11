8a99cbf6b01e5ca889cc2311971b04846ddfc301
Yanella Fernandez Teusen Student <0614131346@my.browardschools.com>
2026-04-11 16:26:06 -0400

    modified calculator p2

--- calculator.py
+++ calculator.py
@@ -9,7 +9,31 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return b / a
+    except:
+        raise ZeroDivisionError
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except:
+        raise ValueError
+
+def exp(a, b):
+    return a ** b
+
 
 
